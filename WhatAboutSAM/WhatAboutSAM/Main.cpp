bf437edd6592cdbf8308023810808d5dd878c1b6
Pedro Gabaldón Juliá <pedro.gabaldon@itresit.es>
2023-08-10 19:46:17 +0200

    First Commit

---
+++ WhatAboutSAM/WhatAboutSAM/Main.cpp
@@ -0,0 +1,66 @@
+// Peter Gabaldon. https://pgj11.com/.
+// Dumping SAM with clean indirect syscalls, custom call stacks, API hashing and walking PEB. Thanks to this resources
+// https://github.com/Maldev-Academy/HellHall
+// https://0xdarkvortex.dev/proxying-dll-loads-for-hiding-etwti-stack-tracing/
+// https://0xdarkvortex.dev/hiding-in-plainsight/
+// https://0xpat.github.io/Malware_development_part_4/
+
+
+#include <Windows.h>
+#include <winternl.h>
+#include <windef.h>
+
+FARPROC myGetProcAddress(PCHAR module, PCHAR export) {
+	PCHAR moduleMayus = (PCHAR)HeapAlloc(GetProcessHeap, 0, strlen(module));
+	CharUpperA(moduleMayus);
+
+	PPEB pPEB = (PPEB)__readgsqword(0x60);
+	PPEB_LDR_DATA pLoaderData = pPEB->Ldr;
+	PLIST_ENTRY listHead = &pLoaderData->InMemoryOrderModuleList;
+	PLIST_ENTRY listCurrent = listHead->Flink;
+	PVOID moduleAddress;
+	do
+	{
+		PLDR_DATA_TABLE_ENTRY dllEntry = CONTAINING_RECORD(listCurrent, LDR_DATA_TABLE_ENTRY, InMemoryOrderLinks);
+		DWORD dllNameLength = WideCharToMultiByte(CP_ACP, 0, dllEntry->FullDllName.Buffer, dllEntry->FullDllName.Length, NULL, 0, NULL, NULL);
+		PCHAR dllName = (PCHAR)HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, dllNameLength);
+		WideCharToMultiByte(CP_ACP, 0, dllEntry->FullDllName.Buffer, dllEntry->FullDllName.Length, dllName, dllNameLength, NULL, NULL);
+		CharUpperA(dllName);
+		if (strstr(dllName, moduleMayus))
+		{
+			moduleAddress = dllEntry->DllBase;
+			HeapFree(GetProcessHeap(), 0, dllName);
+			break;
+		}
+		HeapFree(GetProcessHeap(), 0, dllName);
+		listCurrent = listCurrent->Flink;
+	} while (listCurrent != listHead);
+
+	PIMAGE_DOS_HEADER pDosHeader = (PIMAGE_DOS_HEADER)moduleAddress;
+	PIMAGE_NT_HEADERS pNtHeader = (PIMAGE_NT_HEADERS)((PBYTE)moduleAddress + pDosHeader->e_lfanew);
+	PIMAGE_OPTIONAL_HEADER pOptionalHeader = (PIMAGE_OPTIONAL_HEADER) & (pNtHeader->OptionalHeader);
+	PIMAGE_EXPORT_DIRECTORY pExportDirectory = (PIMAGE_EXPORT_DIRECTORY)((PBYTE)moduleAddress + pOptionalHeader->DataDirectory[IMAGE_DIRECTORY_ENTRY_EXPORT].VirtualAddress);
+	PULONG pAddressOfFunctions = (PULONG)((PBYTE)moduleAddress + pExportDirectory->AddressOfFunctions);
+	PULONG pAddressOfNames = (PULONG)((PBYTE)moduleAddress + pExportDirectory->AddressOfNames);
+	PUSHORT pAddressOfNameOrdinals = (PUSHORT)((PBYTE)moduleAddress + pExportDirectory->AddressOfNameOrdinals);
+
+	FARPROC found = NULL;
+
+	for (int i = 0; i < pExportDirectory->NumberOfNames; ++i) {
+		PCSTR pFunctionName = (PSTR)((PBYTE)moduleAddress + pAddressOfNames[i]);
+		if (strcmp(pFunctionName, export) == 0)
+		{
+			found = (FARPROC)((PBYTE)moduleAddress + pAddressOfFunctions[pAddressOfNameOrdinals[i]]));
+		}
+	}
+	HeapFree(GetProcessHeap, 0, moduleMayus);
+
+	return found;
+}
+
+int main(int argc, char** argv) {
+	typedef FARPROC(WINAPI* myMessageBox)(HWND, LPCTSTR, LPCTSTR, UINT);
+	myMessageBox pMyMessageBox = (myMessageBox)myGetProcAddress((PCHAR)"user32.dll", (PCHAR)"MessageBoxA");
+
+	pMyMessageBox(NULL, (LPCTSTR)"TEST", (LPCTSTR)"TEST", MB_OK);
+}
