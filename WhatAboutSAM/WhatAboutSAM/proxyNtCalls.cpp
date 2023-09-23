329bc1351f3bf1b03e615e13252975e4a231b853
Peter <peter@pgj11.com>
2023-09-23 18:29:58 +0200

    :S

--- WhatAboutSAM/WhatAboutSAM/proxyNtCalls.cpp
+++ WhatAboutSAM/WhatAboutSAM/proxyNtCalls.cpp
@@ -12,12 +12,111 @@ NTSTATUS proxyNtOpenKey (FARPROC pNtOpenKey, PHANDLE KeyHandle, ACCESS_MASK Desi
 	extern VOID CALLBACK WorkCallbackNtOpenKey(PHANDLE KeyHandle, ACCESS_MASK DesiredAccess, POBJECT_ATTRIBUTES ObjectAttributes);
 
 	NTOPENKEY_ARGS ntOpenKeyArgs = {};
+	ntOpenKeyArgs.pNtOpenKey = pNtOpenKey;
 	ntOpenKeyArgs.KeyHandle = KeyHandle;
 	ntOpenKeyArgs.DesiredAccess = DesiredAccess;
 	ntOpenKeyArgs.ObjectAttributes = ObjectAttributes;
 
 	PTP_WORK WorkReturn = NULL;
 	pMyTpAllocWork(&WorkReturn, (PTP_WORK_CALLBACK)WorkCallbackNtOpenKey, &ntOpenKeyArgs, NULL);
+	pMyTpPostWork(WorkReturn);
+	pMyTpReleaseWork(WorkReturn);
+}
+
+NTSTATUS proxyNtQueryKey(FARPROC pNtQueryKey, HANDLE KeyHandle, KEY_INFORMATION_CLASS KeyInformationClass, PVOID KeyInformation, ULONG Length, PULONG ResultLength) {
+	extern VOID CALLBACK WorkCallbackNtQueryKey(HANDLE KeyHandle, KEY_INFORMATION_CLASS KeyInformationClass, PVOID KeyInformation, ULONG Length, PULONG ResultLength);
+
+	NTQUERYKEY_ARGS ntQueryKeyArgs = {};
+	ntQueryKeyArgs.pNtQueryKey = pNtQueryKey;
+	ntQueryKeyArgs.KeyHandle = KeyHandle;
+	ntQueryKeyArgs.KeyInformationClass = KeyInformationClass;
+	ntQueryKeyArgs.KeyInformation = KeyInformation;
+	ntQueryKeyArgs.Length = Length;
+	ntQueryKeyArgs.ResultLength = ResultLength;
+
+	PTP_WORK WorkReturn = NULL;
+	pMyTpAllocWork(&WorkReturn, (PTP_WORK_CALLBACK)WorkCallbackNtQueryKey, &ntQueryKeyArgs, NULL);
+	pMyTpPostWork(WorkReturn);
+	pMyTpReleaseWork(WorkReturn);
+}
+
+NTSTATUS proxyNtEnumerateKey(FARPROC pNtEnumerateKey, HANDLE KeyHandle, ULONG Index, KEY_INFORMATION_CLASS KeyInformationClass, PVOID KeyInformation, ULONG Length, PULONG ResultLength) {
+	extern VOID CALLBACK WorkCallbackNtEnumerateKey(HANDLE KeyHandle, ULONG Index, KEY_INFORMATION_CLASS KeyInformationClass, PVOID KeyInformation, ULONG Length, PULONG ResultLength);
+
+	NTENUMERATEKEY_ARGS ntEnumerateKeyArgs = {};
+	ntEnumerateKeyArgs.pNtEnumerateKey = pNtEnumerateKey;
+	ntEnumerateKeyArgs.KeyHandle = KeyHandle;
+	ntEnumerateKeyArgs.Index = Index;
+	ntEnumerateKeyArgs.KeyInformationClass = KeyInformationClass;
+	ntEnumerateKeyArgs.KeyInformation = KeyInformation;
+	ntEnumerateKeyArgs.Length = Length;
+	ntEnumerateKeyArgs.ResultLength = ResultLength;
+
+	PTP_WORK WorkReturn = NULL;
+	pMyTpAllocWork(&WorkReturn, (PTP_WORK_CALLBACK)WorkCallbackNtEnumerateKey, &ntEnumerateKeyArgs, NULL);
+	pMyTpPostWork(WorkReturn);
+	pMyTpReleaseWork(WorkReturn);
+}
+
+NTSTATUS proxyNtQueryValueKey(FARPROC pNtQueryValueKey, HANDLE KeyHandle, PUNICODE_STRING ValueName, KEY_VALUE_INFORMATION_CLASS KeyValueInformationClass, PVOID KeyValueInformation, ULONG Length, PULONG ResultLength) {
+	extern VOID CALLBACK WorkCallbackNtQueryValueKey(HANDLE KeyHandle, PUNICODE_STRING ValueName, KEY_VALUE_INFORMATION_CLASS KeyValueInformationClass, PVOID KeyValueInformation, ULONG Length, PULONG ResultLength);
+
+	NTQUERYVALUEKEY_ARGS ntQueryValueKeyArgs = {};
+	ntQueryValueKeyArgs.pNtQueryValueKey = pNtQueryValueKey;
+	ntQueryValueKeyArgs.KeyHandle = KeyHandle;
+	ntQueryValueKeyArgs.ValueName = ValueName;
+	ntQueryValueKeyArgs.KeyValueInformationClass = KeyValueInformationClass;
+	ntQueryValueKeyArgs.KeyValueInformation = KeyValueInformation;
+	ntQueryValueKeyArgs.Length = Length;
+	ntQueryValueKeyArgs.ResultLength = ResultLength;
+
+	PTP_WORK WorkReturn = NULL;
+	pMyTpAllocWork(&WorkReturn, (PTP_WORK_CALLBACK)WorkCallbackNtQueryValueKey, &ntQueryValueKeyArgs, NULL);
+	pMyTpPostWork(WorkReturn);
+	pMyTpReleaseWork(WorkReturn);
+}
+
+NTSTATUS proxyNtEnumerateValueKey(FARPROC pNtEnumerateValueKey, HANDLE KeyHandle, ULONG Index, KEY_VALUE_INFORMATION_CLASS KeyValueInformationClass, PVOID KeyValueInformation, ULONG Length, PULONG ResultLength) {
+	extern VOID CALLBACK WorkCallbackNtEnumerateValueKey(HANDLE KeyHandle, ULONG Index, KEY_VALUE_INFORMATION_CLASS KeyValueInformationClass, PVOID KeyValueInformation, ULONG Length, PULONG ResultLength);
+
+	NTENUMERATEVALUEKEY_ARGS ntEnumerateValueKeyArgs = {};
+	ntEnumerateValueKeyArgs.pNtEnumerateValueKey = pNtEnumerateValueKey;
+	ntEnumerateValueKeyArgs.KeyHandle = KeyHandle;
+	ntEnumerateValueKeyArgs.Index = Index;
+	ntEnumerateValueKeyArgs.KeyValueInformationClass = KeyValueInformationClass;
+	ntEnumerateValueKeyArgs.KeyValueInformation = KeyValueInformation;
+	ntEnumerateValueKeyArgs.Length = Length;
+	ntEnumerateValueKeyArgs.ResultLength = ResultLength;
+
+	PTP_WORK WorkReturn = NULL;
+	pMyTpAllocWork(&WorkReturn, (PTP_WORK_CALLBACK)WorkCallbackNtEnumerateValueKey, &ntEnumerateValueKeyArgs, NULL);
+	pMyTpPostWork(WorkReturn);
+	pMyTpReleaseWork(WorkReturn);
+}
+
+NTSTATUS proxyNtCloseKey(FARPROC pNtCloseKey, HANDLE KeyHandle) {
+	extern VOID CALLBACK WorkCallbackNtCloseKey(HANDLE KeyHandle);
+
+	NTCLOSE_ARGS ntCloseKeyArgs = {};
+	ntCloseKeyArgs.pNtCloseKey = pNtCloseKey;
+	ntCloseKeyArgs.KeyHandle = KeyHandle;
+
+	PTP_WORK WorkReturn = NULL;
+	pMyTpAllocWork(&WorkReturn, (PTP_WORK_CALLBACK)WorkCallbackNtCloseKey, &ntCloseKeyArgs, NULL);
+	pMyTpPostWork(WorkReturn);
+	pMyTpReleaseWork(WorkReturn);
+}
+
+NTSTATUS proxyRtlInitUnicodeString(FARPROC pRtlInitUnicodeString, PUNICODE_STRING DestinationString, __drv_aliasesMem PCWSTR SourceString) {
+	extern VOID CALLBACK WorkCallbackRtlInitUnicodeString(PUNICODE_STRING DestinationString, __drv_aliasesMem PCWSTR SourceString);
+
+	RTLINITUNICODESTRING_ARGS rtlInitUnicodeStringArgs = {};
+	rtlInitUnicodeStringArgs.pRltInitUnicodeString = pRtlInitUnicodeString;
+	rtlInitUnicodeStringArgs.DestinationString = DestinationString;
+	rtlInitUnicodeStringArgs.SourceString = SourceString;
+
+	PTP_WORK WorkReturn = NULL;
+	pMyTpAllocWork(&WorkReturn, (PTP_WORK_CALLBACK)WorkCallbackRtlInitUnicodeString, &rtlInitUnicodeStringArgs, NULL);
 	pMyTpPostWork(WorkReturn);
 	pMyTpReleaseWork(WorkReturn);
 }
--- WhatAboutSAM/WhatAboutSAM/proxyNtCalls.h
+++ WhatAboutSAM/WhatAboutSAM/proxyNtCalls.h
@@ -10,14 +10,14 @@
 #include "ntdll.h"
 
 typedef struct _NTOPENKEY_ARGS {
-    UINT_PTR pNtOpenKey;                   // pointer to NtOpenKey - rax
+    FARPROC pNtOpenKey;                   // pointer to NtOpenKey - rax
 	PHANDLE KeyHandle;                     // rcx
     ACCESS_MASK DesiredAccess;             // rdx 
     POBJECT_ATTRIBUTES ObjectAttributes;   // r8
 } NTOPENKEY_ARGS, * PNTOPENKEY_ARGS;
 
 typedef struct _NTQUERYKEY_ARGS {
-    UINT_PTR pNtQueryKey;                                           // pointer to NtQueryKey - rax
+    FARPROC pNtQueryKey;                                           // pointer to NtQueryKey - rax
     HANDLE KeyHandle;                                               // rcx
     KEY_INFORMATION_CLASS KeyInformationClass;                      // rdx
     PVOID KeyInformation;                                           // r8 
@@ -26,7 +26,7 @@ typedef struct _NTQUERYKEY_ARGS {
 } NTQUERYKEY_ARGS, * PNTQUERYKEY_ARGS;
 
 typedef struct _NTENUMERATEKEY_ARGS {
-    UINT_PTR pNtEnumerateKey;                                       // pointer to NtEnumerateKey - rax
+    FARPROC pNtEnumerateKey;                                       // pointer to NtEnumerateKey - rax
     HANDLE KeyHandle;                                               // rcx
     ULONG Index;                                                    // rdx
     KEY_INFORMATION_CLASS KeyInformationClass;                      // r8 
@@ -36,7 +36,7 @@ typedef struct _NTENUMERATEKEY_ARGS {
 } NTENUMERATEKEY_ARGS, * PNTENUMERATEKEY_ARGS;
 
 typedef struct _NTQUERYVALUEKEY_ARGS {
-    UINT_PTR pNtQueryValueKey;                                      // pointer to NtQueryValueKey - rax
+    FARPROC pNtQueryValueKey;                                      // pointer to NtQueryValueKey - rax
     HANDLE KeyHandle;                                               // rcx
     PUNICODE_STRING ValueName;                                      // rdx
     KEY_VALUE_INFORMATION_CLASS KeyValueInformationClass;           // r8 
@@ -46,7 +46,7 @@ typedef struct _NTQUERYVALUEKEY_ARGS {
 } NTQUERYVALUEKEY_ARGS, * PNTQUERYVALUEKEY_ARGS;
 
 typedef struct _NTENUMERATEVALUEKEY_ARGS {
-    UINT_PTR pNtEnumerateValueKey;                                  // pointer to NtEnumerateValueKey - rax
+    FARPROC pNtEnumerateValueKey;                                  // pointer to NtEnumerateValueKey - rax
     HANDLE KeyHandle;                                               // rcx
     ULONG Index;                                                    // rdx
     KEY_VALUE_INFORMATION_CLASS KeyValueInformationClass;           // r8 
@@ -56,12 +56,12 @@ typedef struct _NTENUMERATEVALUEKEY_ARGS {
 } NTENUMERATEVALUEKEY_ARGS, * PNTENUMERATEVALUEKEY_ARGS;
 
 typedef struct _NTCLOSE_ARGS {
-    UINT_PTR pNtCloseKey;                                           // pointer to pNtCloseKey - rax
+    FARPROC pNtCloseKey;                                           // pointer to pNtCloseKey - rax
     HANDLE KeyHandle;                                               // rcx
 } NTCLOSE_ARGS, * PNTCLOSE_ARGS;
 
 typedef struct _RTLINITUNICODESTRING_ARGS {
-    UINT_PTR pRltInitUnicodeString;                                 // pointer to RltInitUnicodeString - rax
+    FARPROC pRltInitUnicodeString;                                 // pointer to RltInitUnicodeString - rax
     PUNICODE_STRING DestinationString;                              // rcx
     __drv_aliasesMem PCWSTR SourceString;                           // rdx
 } RTLINITUNICODESTRING_ARGS, * PRTLINITUNICODESTRING_ARGS;
